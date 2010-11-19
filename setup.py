263ec5e6ccb2eba4f2ed07df6b3907c5fb4dfa7e
Virgil Dupras <hsoft@hardcoded.net>
2010-11-19 14:45:07 +0100

    v1.0.4
    --HG--
    branch : py3k

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ LONG_DESC = open('README', 'rt').read() + '\n\n' + open('CHANGES', 'rt').read()
 
 setup(
     name='hsaudiotag3k',
-    version='1.0.3',
+    version='1.0.4',
     author='Hardcoded Software',
     author_email='hsoft@hardcoded.net',
     packages=['hsaudiotag'],
     scripts=[],
-    install_requires=['hsutil3k>=1.0.1', 'py>=1.3.2'],
+    install_requires=['hsutil3k>=1.0.2', 'py>=1.3.4'],
     url='http://hg.hardcoded.net/hsaudiotag/',
     license='BSD License',
     description='Read metdata (tags) of mp3, mp4, wma, ogg, flac and aiff files.',
